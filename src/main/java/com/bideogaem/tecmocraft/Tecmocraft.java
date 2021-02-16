e7d7f7cdc22902a7ca9a6d6f96c047d5806474ca
swimdoctor <43818006+swimdoctor@users.noreply.github.com>
2021-02-16 15:56:22 -0800

    Add files via upload

--- src/main/java/com/bideogaem/tecmocraft/Tecmocraft.java
+++ src/main/java/com/bideogaem/tecmocraft/Tecmocraft.java
@@ -1,11 +1,15 @@
 package com.bideogaem.tecmocraft;
 
+import com.bideogaem.tecmocraft.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 public class Tecmocraft implements ModInitializer {
+
+    public static final String MOD_ID = "tutorial";
+
     @Override
     public void onInitialize() {
-
+        ModItems.registerItems();
     }
 
 }
